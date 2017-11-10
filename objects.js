99df7e9d900be013cf15fca4f21d1f6700dc15d7
Patrick McCarty <patrickthesailorman@gmail.com>
2017-11-10 01:36:44 +0000

    first commit

---
+++ objects.js
@@ -0,0 +1,11 @@
+var hotel = {
+  name: 'Biltmore',
+  rooms: 500,
+  roomsBooked: 150,
+  roomService: true,
+  pool: false, 
+     
+roomsAvail: function() {
+    return this.rooms - this.roomsBooked;
+    }     
+ }
